0a4186b5afab4948001879322cc3aea3560a834e
Tim Otto <tim.otto@ubergrund.de>
2017-03-24 13:38:12 +0100

    Created mock update UI work flow

--- app/scripts/controllers/update.js
+++ app/scripts/controllers/update.js
@@ -8,10 +8,54 @@
  * Controller of the flampeFrontendAngularApp
  */
 angular.module('flampeFrontendAngularApp')
-  .controller('UpdateCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+  .controller('UpdateCtrl', ['$scope','$timeout',function ($scope,$timeout) {
+    $scope.status = 'UPDATE_CONTENT_NO_UPDATES';
+    $scope.textValues = {
+      version: '123.456'
+    };
+    $scope.showCheck = true;
+    $scope.showUpdate = false;
+    $scope.showProgress = false;
+    $scope.enableCheck = true;
+    $scope.enableUpdate = true;
+    $scope.enableProgress = true;
+
+    $scope.check = function() {
+      if ($scope.status === 'UPDATE_CONTENT_INSTALLING') {
+        return;
+      } else {
+        var butThisTime = $scope.status === 'UPDATE_CONTENT_NO_UPDATES';
+        $scope.status = 'UPDATE_CONTENT_CHECKING';
+        $scope.enableCheck = false;
+        $timeout(function(){
+          if (butThisTime) {
+            $scope.status = 'UPDATE_CONTENT_UPDATES';
+            $scope.showCheck = false;
+            $scope.enableCheck = true;
+            $scope.showUpdate = true;
+            $scope.enableUpdate = true;
+          } else {
+            $scope.status = 'UPDATE_CONTENT_NO_UPDATES';
+            $scope.showCheck = true;
+            $scope.enableCheck = true;
+            $scope.showUpdate = false;
+            $scope.enableUpdate = true;
+          }
+        }, 1000);
+      }
+    };
+    $scope.doUpdate = function() {
+      $scope.status = 'UPDATE_CONTENT_INSTALLING';
+      $scope.enableUpdate = false;
+      $scope.enableCheck = true;
+      $scope.showCheck = true;
+      $scope.showProgress = true;
+      $timeout(function(){
+        $scope.status = 'UPDATE_CONTENT_INSTALLED';
+        $scope.enableUpdate = true;
+        $scope.showUpdate = false;
+        $scope.showProgress = false;
+        $scope.textValues.version = parseInt(1+Math.random()*1000)+'.'+parseInt(Math.random()*10000);
+      }, 10000);
+    };
+  }]);
