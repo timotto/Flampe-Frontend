2abcbefbbdc64d8e93a5fc0e60f0a8353866d29d
Tim Otto <tim.otto@ubergrund.de>
2017-03-24 08:37:35 +0100

    mark active nav-menu-item

--- app/scripts/directives/flnavmenuitem.js
+++ app/scripts/directives/flnavmenuitem.js
@@ -7,12 +7,20 @@
  * # flNavMenuItem
  */
 angular.module('flampeFrontendAngularApp')
-  .directive('flNavMenuItem', function () {
+  .directive('flNavMenuItem', ['$location', function ($location) {
+    function link(scope){
+      scope.currentItem = function(itemLink) {
+        if (!(itemLink !== undefined)) return false;
+        var pot = '#!'+$location.path();
+        return itemLink === pot;
+      };
+    }
     return {
       templateUrl: 'views/nav-menu-item.html',
       restrict: 'E',
       scope: {
         item: '='
-      }
+      },
+      link: link
     };
-  });
+  }]);
