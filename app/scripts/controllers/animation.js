bd90ffb39b1be6dc0d4d9462874af21f895af669
Tim Otto <tim.otto@ubergrund.de>
2017-03-24 11:52:50 +0100

    fixed animation page, added palette options to animations

--- app/scripts/controllers/animation.js
+++ app/scripts/controllers/animation.js
@@ -9,8 +9,27 @@
  */
 angular.module('flampeFrontendAngularApp')
   .controller('AnimationCtrl', ['$scope',function ($scope) {
-    $scope.animationCurrent = '';
-    $scope.animationIdle = '';
+    $scope.animation = {};
+
+    $scope.animations = [
+      {textkey:'ANIM_SOLID'},
+      {textkey:'ANIM_GRADIENT'},
+      {textkey:'ANIM_FIRE'},
+      {textkey:'ANIM_PULSE'},
+      {textkey:'ANIM_JUGGLE'},
+      {textkey:'ANIM_RAINBOW'}
+    ];
+
+    $scope.palettes = [
+      {textkey:'PALETTE_BASE'},
+      {textkey:'PALETTE_HEAT'},
+      {textkey:'PALETTE_OCEAN'},
+      {textkey:'PALETTE_CLOUD'},
+      {textkey:'PALETTE_FOREST'},
+      {textkey:'PALETTE_LAVA'},
+      {textkey:'PALETTE_RAINBOW'},
+      {textkey:'PALETTE_PARTY'}
+    ];
 
     this.awesomeThings = [
       'HTML5 Boilerplate',
